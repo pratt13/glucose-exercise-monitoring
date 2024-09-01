f82270fa15236a447702ed7bfb9aeb91f9faa1ac
Pete Pratt <ppratt@caci.co.uk>
2024-09-01 13:10:19 +0100

    wip

--- diabetes_backend/src/constants.py
+++ diabetes_backend/src/constants.py
@@ -63,6 +63,8 @@ class TABLE_SCHEMA:
             "libre_id",
             "glucose",
             "timestamp",
+            "activity_start",
+            "activity_end",
             "activity_type",
             "seconds_since_start",
         ],
--- diabetes_backend/src/data.py
+++ diabetes_backend/src/data.py
@@ -29,36 +29,27 @@ class Data:
         last_strava_libre_record = self.db_manager.get_last_record(
             DATA_TYPES.STRAVA_LIBRE
         )
-        logger.debug(f"last_strava_libre_record: {last_strava_libre_record}")
         checked_strava_id = last_strava_libre_record[2]
         unchecked_strava_records = self.db_manager.get_filtered_by_id_records(
             DATA_TYPES.STRAVA, checked_strava_id
         )
-        logger.debug("-" * 100)
-        logger.debug(f"unchecked_strava_records: {unchecked_strava_records}")
         new_entry_id = last_strava_libre_record[1]
         # Iterate through the unchecked records in the libre db to find them either side of a window
         for unchecked_strava_record in unchecked_strava_records:
-            start_time = unchecked_strava_record[5]
-            end_time = unchecked_strava_record[6]
+            activity_start_time = unchecked_strava_record[5]
+            activity_end_time = unchecked_strava_record[6]
+            start_time = activity_start_time - timedelta(seconds=3600)
+            end_time = activity_end_time + timedelta(seconds=3600)
             activity_type = unchecked_strava_record[2]
             current_strava_id = unchecked_strava_record[0]
             libre_records = self.db_manager.get_records(
                 DATA_TYPES.LIBRE, start_time, end_time
             )
-            logger.debug(
-                f"Found {len(libre_records)} to add between {start_time} and {end_time}"
-            )
             for libre_record in libre_records:
                 current_timestamp = libre_record[2]
                 current_glucose = libre_record[1]
                 current_libre_id = libre_record[0]
-                # timestamp_since_start = (
-                #     convert_str_to_ts(current_timestamp, DATETIME_FORMAT)
-                #     - convert_str_to_ts(start_time, "%Y-%m-%dT%H:%M:%SZ")
-                # ).total_seconds()
                 timestamp_since_start = (current_timestamp - start_time).total_seconds()
-
                 new_records.append(
                     (
                         new_entry_id,
@@ -66,6 +57,8 @@ class Data:
                         current_libre_id,
                         current_glucose,
                         current_timestamp,
+                        activity_start_time,
+                        activity_end_time,
                         activity_type,
                         timestamp_since_start,
                     )
