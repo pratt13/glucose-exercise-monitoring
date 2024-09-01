bd19f3593349136f93e7f921cf97040b67f4af83
Pete Pratt <ppratt@caci.co.uk>
2024-09-01 15:46:15 +0100

    Feature: Add method to wquery via id
    In the future we will need to query the db by id, add a method to do
    this.

--- diabetes_backend/src/database_manager.py
+++ diabetes_backend/src/database_manager.py
@@ -54,14 +54,17 @@ class PostgresManager:
         conn.close()
 
     def _validate_data_type(self, data_type):
-        if data_type not in (DATA_TYPES.LIBRE, DATA_TYPES.STRAVA):
+        if data_type not in (
+            DATA_TYPES.LIBRE,
+            DATA_TYPES.STRAVA,
+        ):
             raise ValueError(f"Invalid data_type {data_type}")
 
     def _get_default_last_record(self, data_type):
         self._validate_data_type(data_type)
         if data_type == DATA_TYPES.LIBRE:
             return (datetime.datetime(1970, 7, 11, 19, 45, 55), 0)
-        if data_type == DATA_TYPES.STRAVA:
+        elif data_type == DATA_TYPES.STRAVA:
             return [datetime.datetime(1970, 7, 11, 19, 45, 55)]
 
     def get_last_record(self, data_type):
@@ -112,6 +115,34 @@ class PostgresManager:
                         time_field=sql.Identifier(TABLE_SCHEMA.TIME_FIELD[data_type]),
                         end_time=sql.Literal(end_time),
                         start_time=sql.Literal(start_time),
+                    )
+                )
+                res = curs.fetchall()
+
+        # leaving contexts doesn't close the connection
+        conn.close()
+
+        return res or []
+
+    def get_filtered_by_id_records(self, data_type, id):
+        """Fetch the records greater than the given id"""
+        logging.debug(f"get_filtered_by_id_records({data_type})")
+        self._validate_data_type(data_type)
+        logger.debug(f"Retrieving data from {TABLE_SCHEMA.NAME[data_type]}")
+        conn = psycopg2.connect(**self.conn_params)
+        with conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    sql.SQL(
+                        "SELECT {table_columns} FROM {table} WHERE {id_field} > {id_value} ORDER BY {order_by} "
+                    ).format(
+                        table_columns=sql.SQL(", ").join(
+                            map(sql.Identifier, TABLE_SCHEMA.COLUMNS[data_type])
+                        ),
+                        order_by=sql.Identifier(TABLE_SCHEMA.ORDER_BY[data_type]),
+                        table=sql.Identifier(TABLE_SCHEMA.NAME[data_type]),
+                        id_field=sql.Identifier(TABLE_SCHEMA.INDEX_FIELD[data_type]),
+                        id_value=sql.Literal(id),
                     )
                 )
                 res = curs.fetchall()
